ebfbee3c45afa8de3de3aa9c85820383dc328340
shashwat558 <jainshashwat528@gmail.com>
2025-06-05 20:32:01 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Hero from "@/components/Hero";
-import IntroAnimation from "@/components/IntroAnimation";
+
 import LifeUpdates from "@/components/LifeUpdates";
 import { NavDock } from "@/components/NavDock";
 // import OpenSourceContributions from "@/components/OpenSource";
@@ -14,24 +14,20 @@ import TechTags from "@/components/TechStack";
 
 
 import Toaster from "@/components/Toaster";
-import { AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+
 
 
 export default function Home() {
-  const [showIntro, setShowIntro] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => setShowIntro(false), 2500);
+  // const [showIntro, setShowIntro] = useState(true);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setShowIntro(false), 2500);
 
-    return () => clearTimeout(timer)
-  },[]);
+  //   return () => clearTimeout(timer)
+  // },[]);
 
   return (
    <>
-    <AnimatePresence>
-      {showIntro && <IntroAnimation />}
-    </AnimatePresence>
-   {!showIntro && <>
+    
     
     <NavDock />
     <Hero />
@@ -44,8 +40,8 @@ export default function Home() {
     
     <Toaster />
      </>
-    }
     
-   </>
+    
+   
   );
 }
